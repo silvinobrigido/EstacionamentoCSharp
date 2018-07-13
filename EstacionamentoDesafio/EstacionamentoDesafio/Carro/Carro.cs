19c1f7ec3f10026a1b156a2abe213ef568100528
silbapps <silvinobrigido@gmail.com>
2018-07-12 23:59:28 -0300

    ajustes

---
+++ EstacionamentoDesafio/EstacionamentoDesafio/Carro/Carro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionamentoDesafio.Carro
+{
+    class Carro
+    {
+        private String placa;
+        private String marca;
+        private String modelo;
+        private Int32 vaga;
+
+        public string Placa { get => placa; set => placa = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public int Vaga { get => vaga; set => vaga = value; }
+    }
+}
